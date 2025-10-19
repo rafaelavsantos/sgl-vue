0d972766a27ea6a3c4ef9f94f9323be9d37d70e4
Rafaela Santos <73187763+rafaelavsantos@users.noreply.github.com>
2025-10-19 17:18:51 -0300

    Revert "feat: completed the authentication screens"

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import authRoutes from './authRoutes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    authRoutes,
-  ],
+  routes: [],
 })
 
 export default router
